b562b84f498dd9d24aaa8984f2c3f2e10ec161b9
Mitchellliou <mliou@vt.edu>
2018-02-17 17:37:23 -0500

    Firebase unique items

--- app/src/main/java/com/aelleon/theroommateapp/MainActivity.java
+++ app/src/main/java/com/aelleon/theroommateapp/MainActivity.java
@@ -1,18 +1,21 @@
 package com.aelleon.theroommateapp;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.firebase.ui.auth.AuthUI;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
     private EditText message;
-
-    //Authentication
-
 
     //Database
     private FirebaseDatabase database;
@@ -23,16 +26,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        database = FirebaseDatabase.getInstance();
-        myRef = database.getReference("message");
+
+
 
         message = (EditText) findViewById(R.id.message);
 
     }
-    // This is Mitchell's branch
-    public void onButton(View view) {
 
+    public void onButton(View view) {
+        database = FirebaseDatabase.getInstance();
+        myRef = database.getReference("message").child("User").push();
         String mess = message.getText().toString();
+
+
+        Context context = getApplicationContext();
+        CharSequence text = "Hello toast!";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, myRef.toString(), duration);
+        toast.show();
+
         myRef.setValue(mess);
     }
 }
