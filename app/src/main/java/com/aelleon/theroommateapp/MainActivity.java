1887662d6fc8886e95230da880fb47f217b48c4d
Mitchellliou <mliou@vt.edu>
2018-02-17 13:01:13 -0500

    Added basic Firebase database functionality

--- app/src/main/java/com/aelleon/theroommateapp/MainActivity.java
+++ app/src/main/java/com/aelleon/theroommateapp/MainActivity.java
@@ -2,22 +2,37 @@ package com.aelleon.theroommateapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText message;
+
+    //Authentication
+
+
+    //Database
+    private FirebaseDatabase database;
+    private DatabaseReference myRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        database = FirebaseDatabase.getInstance();
+        myRef = database.getReference("message");
 
-        // Write a message to the database
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("message");
+        message = (EditText) findViewById(R.id.message);
 
-        myRef.setValue("Hello, World!");
+    }
+
+    public void onButton(View view) {
+
+        String mess = message.getText().toString();
+        myRef.setValue(mess);
     }
 }
